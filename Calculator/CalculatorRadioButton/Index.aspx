cb3d94b4a15d39871ce8aa4fb6ead7c45919ca8e
Ismail Hossain <adnanhossain9647@gmail.com>
2021-09-07 02:42:07 +0600

    first commit

---
+++ Calculator/CalculatorRadioButton/Index.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CalculatorRadioButton.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Radio Button</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Calculator</h1>
+            
+        </div>
+        <p>
+            <asp:Label ID="lblDisplay" runat="server" Font-Bold="True" Font-Size="Medium"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Number 1"></asp:Label>
+            <asp:TextBox ID="txtNumber1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Number 2"></asp:Label>
+            <asp:TextBox ID="txtNumber2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:RadioButton ID="rbAddition" runat="server" Text="Add" GroupName="Calculator" AutoPostBack="True" OnCheckedChanged="rbAddition_CheckedChanged" />
+            <asp:RadioButton ID="rbSubtraction" runat="server" Text="Subtraction" GroupName="Calculator" AutoPostBack="True" OnCheckedChanged="rbSubtraction_CheckedChanged" />
+            <asp:RadioButton ID="rbMultiplication" runat="server" Text="Multiplication" GroupName="Calculator" AutoPostBack="True" OnCheckedChanged="rbMultiplication_CheckedChanged" />
+            <asp:RadioButton ID="rbDivision" runat="server" Text="Division" GroupName="Calculator" AutoPostBack="True" OnCheckedChanged="rbDivision_CheckedChanged"/>
+        </p>
+        <p>
+            <%--<asp:Button ID="Button1" runat="server" Font-Bold="True" Font-Size="Medium" Text="Calculate" OnClick="Button1_Click" />--%>
+        </p>
+        <hr />
+        <asp:Label ID="lblResult" runat="server" Font-Bold="True" Font-Size="Medium"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Calculator/CalculatorRadioButton/Index.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorRadioButton
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        double number1;
+        double number2;
+        double result;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           if(rbAddition.Checked == true)
+            {
+                number1 = double.Parse(txtNumber1.Text);
+                number2 = double.Parse(txtNumber2.Text);
+
+                result = number1 + number2;
+
+                lblResult.Text = "The result is: " + result.ToString();
+                lblResult.ForeColor = System.Drawing.Color.Blue;
+                lblDisplay.Text = "Addition";
+                lblDisplay.ForeColor = System.Drawing.Color.Blue;
+            }
+            else if(rbSubtraction.Checked == true)
+            {
+                number1 = double.Parse(txtNumber1.Text);
+                number2 = double.Parse(txtNumber2.Text);
+
+                result = number1 - number2;
+
+                lblResult.Text = "The result is: " + result.ToString();
+                lblResult.ForeColor = System.Drawing.Color.Red;
+                lblDisplay.Text = "Subtraction";
+                lblDisplay.ForeColor = System.Drawing.Color.Red;
+            }
+            else if(rbMultiplication.Checked == true)
+            {
+                number1 = double.Parse(txtNumber1.Text);
+                number2 = double.Parse(txtNumber2.Text);
+
+                result = number1 * number2;
+
+                lblResult.Text = "The result is: " + result.ToString();
+                lblResult.ForeColor = System.Drawing.Color.Green;
+                lblDisplay.Text = "Multiplication";
+                lblDisplay.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                number1 = double.Parse(txtNumber1.Text);
+                number2 = double.Parse(txtNumber2.Text);
+
+                result = number1 / number2;
+
+                lblResult.Text = "The result is: " + result.ToString();
+                lblResult.ForeColor = System.Drawing.Color.DarkViolet;
+                lblDisplay.Text = "Division";
+                lblDisplay.ForeColor = System.Drawing.Color.DarkViolet;
+            }
+        }
+
+        protected void rbAddition_CheckedChanged(object sender, EventArgs e)
+        {
+            number1 = double.Parse(txtNumber1.Text);
+            number2 = double.Parse(txtNumber2.Text);
+
+            result = number1 + number2;
+
+            lblResult.Text = "The result is: " + result.ToString();
+            lblResult.ForeColor = System.Drawing.Color.Blue;
+            lblDisplay.Text = "Addition";
+            lblDisplay.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        protected void rbSubtraction_CheckedChanged(object sender, EventArgs e)
+        {
+            number1 = double.Parse(txtNumber1.Text);
+            number2 = double.Parse(txtNumber2.Text);
+
+            result = number1 - number2;
+
+            lblResult.Text = "The result is: " + result.ToString();
+            lblResult.ForeColor = System.Drawing.Color.Red;
+            lblDisplay.Text = "Subtraction";
+            lblDisplay.ForeColor = System.Drawing.Color.Red;
+        }
+
+        protected void rbMultiplication_CheckedChanged(object sender, EventArgs e)
+        {
+            number1 = double.Parse(txtNumber1.Text);
+            number2 = double.Parse(txtNumber2.Text);
+
+            result = number1 * number2;
+
+            lblResult.Text = "The result is: " + result.ToString();
+            lblResult.ForeColor = System.Drawing.Color.Green;
+            lblDisplay.Text = "Multiplication";
+            lblDisplay.ForeColor = System.Drawing.Color.Green;
+        }
+
+        protected void rbDivision_CheckedChanged(object sender, EventArgs e)
+        {
+            number1 = double.Parse(txtNumber1.Text);
+            number2 = double.Parse(txtNumber2.Text);
+
+            result = number1 / number2;
+
+            lblResult.Text = "The result is: " + result.ToString();
+            lblResult.ForeColor = System.Drawing.Color.DarkViolet;
+            lblDisplay.Text = "Division";
+            lblDisplay.ForeColor = System.Drawing.Color.DarkViolet;
+        }
+    }
+}
